1b98aa54f00229a976c5fa8c5770733a84bdb1ac
Maxx Garcia <maxx.v.garcia@gmail.com>
2018-08-09 12:38:51 -0600

    added all of the tokenauth code to the server, should be good go

---
+++ routes/admin.js
@@ -0,0 +1,27 @@
+const Showtunes = require("../models/songs");
+const express = require("express");
+
+const adminRoutes = express.Router();
+
+adminRoutes.route("/")
+    .post((req, res) => {
+        let newSong = new Showtunes(req.body);
+        newSong.user = req.user._id
+        newSong.save((err, newSong) => {
+            return err ? res.status(500).send(err) : res.status(201).send(newSong);
+        })
+    })
+
+adminRoutes.route("/:id")
+    .put((req, res) => {
+        Showtunes.findbyIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedSong) => {
+            return err ? res.status(500).send(err) : res.status(200).send(updatedSong);
+        })
+    })
+    .delete((req, res) => {
+        Showtunes.findByIdAndRemove(req.params.id, (err) => {
+            return err ? res.status(500).send(err) : res.status(200).send("THE ROUGE SHOWTUNE HAS BEEN ELIMINATED")
+        })
+    })
+
+module.exports = adminRoutes;
