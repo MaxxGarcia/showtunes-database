f53ec772fb6c7507acb109352614b8be8edbe18a
Nathan Wengert <nate.weng@gmail.com>
2018-08-13 15:47:21 -0600

    sticky searchbar

--- client/src/Routes/Results.js
+++ client/src/Routes/Results.js
@@ -3,11 +3,10 @@ import Footer from '../Footer.js';
 import Ads from '../Ads.js';
 
 function Results(props) {
-    console.log(this.props)
     return (
         <div id="resultsDiv">
             <div id="songInfoDiv">
-                <p>Song: This will be pulled from props <br/>
+                <p id="songInfoP">Song: This will be pulled from props <br/>
                 Show: Ragtime <br/>
                 Years: 1992 - 1994 <br/>
                 Duet MF</p>
@@ -35,6 +34,10 @@ function Results(props) {
                     <div className="recommendation">Recommended Song</div>
                     <div className="recommendation">Recommended Song</div>
                     <div className="recommendation">Recommended Song</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quasi dolorum officia ab architecto veritatis asperiores tempore sint, consequuntur quisquam sequi necessitatibus dolorem minima incidunt nihil dolore. Numquam, totam dicta?</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni esse pariatur explicabo quibusdam ab similique dolorum nobis, eum odio nihil sequi, asperiores, quam omnis expedita corrupti id? Ipsum, qui aperiam.</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus perspiciatis repellat dolore quia fugiat fuga totam officiis mollitia. Nostrum consequuntur voluptates tempore saepe maxime corrupti assumenda perferendis ullam cum at?</p>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, blanditiis eum, aliquam sint in unde dolores saepe ullam eveniet laudantium dignissimos fuga odio nobis eaque accusamus! Dolorem, sed aliquid? Non!</p>
                 </div>
             </div>
             <Footer />
