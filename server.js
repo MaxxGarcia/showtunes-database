369d7e829b8217f9009705857c5704f940e6f051
Maxx Garcia <maxx.v.garcia@gmail.com>
2018-08-20 16:05:12 -0600

    finalized

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+const path = require("path")
+require("dotenv").config()
+
 
 //Auth
 require("dotenv").config();
 const expressJWT = require("express-jwt");
 
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const MONGODB_URI = 'mongodb://localhost:27017/showtunes';
 const app = express();
 
 app.use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: false }))
+    app.use(express.static(path.join(__dirname, "client", "build")))
     .use("/private", expressJWT({ secret: process.env.SECRET }))
     .use("/private/admin", require("./routes/admin"))
     .use('/songs', require('./routes/songs'))
@@ -25,8 +29,11 @@ app.use(bodyParser.json())
     });
 
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGOLAB_GOLD_URI, { useNewUrlParser: true })
     .then((db) => console.log("DB is Online!"))
     .catch(err => console.log(err));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
 app.listen(PORT, () => console.log(`Connected to showtunes Server on ${PORT}`));
