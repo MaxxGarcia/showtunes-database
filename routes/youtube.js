2f3e4e2d421fa454e3fc96dc0295165b9b59d52a
Maxx Garcia <maxx.v.garcia@gmail.com>
2018-08-20 16:52:30 -0600

    hiding secrets

--- routes/youtube.js
+++ routes/youtube.js
@@ -2,16 +2,13 @@ const express = require("express");
 const axios = require("axios");
 const youtubeRoutes = express.Router();
 
-var API_KEY = "***REMOVED***"
-
-
 
 youtubeRoutes.post("/", (req, res) => {
     let authOptions = {
         url: `https://www.googleapis.com/youtube/v3/search`,
         method: "GET",
         params: {
-            key: API_KEY,
+            key: process.env.GOOGLE_KEY,
             maxResults: 1,
             part: "snippet",
             q: req.body.q
